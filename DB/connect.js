b676681a55d05f7d798784f811bb0b4da47f3bef
vaishnavi2662 <kharchevaishnavi1@gmail.com>
2024-08-03 17:19:18 +0530

    Update connect.js

--- DB/connect.js
+++ DB/connect.js
@@ -8,7 +8,7 @@ dotenv.config(
 const connectDB=async()=>{
    
     console.log(process.env.MONGOOSE_URI);
-        const connectionInstance=await mongoose.connect(`${process.env.MONGOOSE_URI}}/${"expenses"}`)
+        const connectionInstance=await mongoose.connect("mongodb+srv://kharchevaishnavi1:XA3udRWguBd6w5pe@cluster0.ml88kni.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/expenses")
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
   
 }
